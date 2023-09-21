8319aff455c37d3bf00c8d7c1b67ce9174f66516
Anar Habib <anarhabibullayev3@gmail.com>
2023-09-21 18:55:23 +0400

    newPOst  page created

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import PostsPage from "./pages/posts/posts";
 import PostDetails from "./pages/postDetail/postDetails";
+import CreatePost from "./pages/newPost/createPost";
 import { route } from "./route";
 
 window.customElements.define("posts-page", PostsPage);
 window.customElements.define("post-details", PostDetails);
+window.customElements.define("create-post", CreatePost);
 
 window.addEventListener("load", handleRouteChange);
 window.addEventListener("popstate", handleRouteChange);
@@ -13,6 +15,7 @@ const appContainer = document.getElementById("root");
 const routes = {
   "/": PostsPage,
   "/details": PostDetails,
+  "/create": CreatePost,
 };
 
 function renderComponent(routePath) {
@@ -38,6 +41,8 @@ if (initialRoute === "/") {
   renderComponent(PostsPage);
 } else if (initialRoute === "/details") {
   renderComponent(PostDetails);
+} else if (initialRoute === "/create") {
+  renderComponent(CreatePost);
 }
 
 route("/", () => renderComponent(PostsPage));
